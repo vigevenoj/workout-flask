218205a573e34ac98592603bcc7796f8959e07f9
Jacob Vigeveno <vigevenoj@gmail.com>
2017-04-04 15:28:19 -0700

    improve handling

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
-from flask import Flask
+from flask import Flask, abort, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from flask import abort
 import os
+from models import *
 # import yaml
 
 app = Flask(__name__)
@@ -11,13 +11,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.route('/')
 def index():
-    return "Hello world"
+    return "Hello world\n"
 
 
 @app.route('/running/api/v1/runs', methods=['GET'])
 def get_runs():
+    return jsonify({'runs': Run.query.all()})
     # for now, abort with a 501 not implemented
     abort(501)
 
@@ -25,7 +36,7 @@ def get_runs():
 @app.route('/running/api/v1/runs/<int:run_id>', methods=['GET'])
 def get_run(run_id):
     # for now abort with a 501 not implemented
-    abort(501)
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
