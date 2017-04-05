14b369bb09fb1ab462300ede395b66436a50f194
Jacob Vigeveno <vigevenoj@gmail.com>
2017-04-04 22:58:16 -0700

    wire up posting new runs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from flask import Flask, abort, jsonify, make_response
+from flask import Flask, abort, jsonify, make_response, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 from models import *
@@ -37,6 +37,26 @@ def get_runs():
 def get_run(run_id):
     # for now abort with a 501 not implemented
     abort(404)
+
+
+@app.route('/running/api/v1/runs', methods=['POST'])
+# @auth.login_required
+def create_run():
+    if not request.json:
+        abort(400)
+    rdate = request.json['rdate']
+    timeofday = request.json['timeofday']
+    distance = request.json['distance']
+    units = request.json['units']
+    elapsed = request.json['elapsed']
+#    effort = request.json['effort']
+    comment = request.json['comment']
+    run = Run(rdate=rdate, timeofday=timeofday, distance=distance, units=units,
+              elapsed=elapsed, effort="", comment=comment)
+    db.session.add(run)
+    db.session.commit()
+    return (jsonify({'run': run.rdate}), 201,
+            {'Location': url_for('get_run', run_id=run.runid, _external=True)})
 
 if __name__ == '__main__':
     app.run(debug=True)
